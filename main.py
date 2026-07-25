0259c5a14d55667df0ff520a0b8bd9b072c5ec07
Naisha Dey <naishadey@Naishas-MacBook-Air.local>
2026-07-24 22:40:44 -0500

    some fun little regression models

--- main.py
+++ main.py
@@ -1,62 +1,56 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-from sklearn.model_selection import train_test_split
-from sklearn import datasets, linear_model, metrics
-from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split, GridSearchCV
+from sklearn.preprocessing import StandardScaler, LabelEncoder
 from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score, log_loss
+# import tensorflow as tf
+# from tensorflow import keras
+# from tensorflow.keras import layers
+# from tensorflow.keras.models import Sequential
+# from torch import nn
+from sklearn.metrics import classification_report, confusion_matrix
+from sklearn.utils.class_weight import compute_sample_weight
+import statsmodels.api as sm
+import mord
 
 df = pd.read_csv('new_emergency.csv')
 print(df.shape)
 
-X = df.iloc[:, 7: ]
+X = df.iloc[:, df.columns.get_loc('cc_abdominaldistention'): ]
 y = df['esi']
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+le = LabelEncoder()
+y_encoded = le.fit_transform(y)
 
-# reg = linear_model.LogisticRegression(max_iter=10000, random_state=0)
-# reg.fit(X_train, y_train)
+X_train, X_test, y_train, y_test = train_test_split(X, y_encoded, test_size=0.2, random_state=42, stratify = y_encoded)
 
-# y_pred = reg.predict(X_test)
+#Multinomial Logistic Regression Model
+# model = LogisticRegression(solver="saga", max_iter=1000, random_state=42, l1_ratio=1, class_weight='balanced')
+# model.fit(X_train, y_train)
 
-scaler  = StandardScaler()
-X_train = scaler.fit_transform(X_train)
-X_test  = scaler.transform(X_test)
+# y_pred = model.predict(X_test)
+# y_pred_proba = model.predict_proba(X_test)
 
-log_model = LogisticRegression(max_iter = 3000)
-log_model.fit(X_train, y_train)
+# test_accuracy = accuracy_score(y_test, y_pred)
+# train_accuracy = model.score(X_train, y_train)
+# logloss = log_loss(y_test, y_pred_proba)
 
-y_pred = log_model.predict(X_test)
+# print('Training Accuracy', train_accuracy)
+# print('Test Accuracy:', test_accuracy, 'Log Loss:', logloss)
 
-#y_pred = reg.predict(X_test)
+# print(classification_report(y_test, y_pred, target_names=[str(c) for c in le.classes_]))
+# print(confusion_matrix(y_test, y_pred))
 
-print(f"Logistic Regression model accuracy: {metrics.accuracy_score(y_test, y_pred) * 100:.2f}%")
+#Ordinal Logistic Regression Model
+model = mord.LogisticAT(alpha=30.0)
+sample_weights = compute_sample_weight(class_weight='balanced', y=y_train)
 
-def sigmoid(x):
-    arr = np.array(x)
-    return 1/(1+((np.e)**(-arr)))
+model.fit(X_train, y_train, sample_weight=sample_weights)
 
+y_pred = model.predict(X_test)
 
-def train_logistic_regression_L2(X, y, lr=0.1, steps=1000, C=1.0):
-    n_samples, n_features = X.shape
-    lam = 1/(2*n_samples)
-    w = np.zeros(n_features)
-    b = 0.0
-    for i in range(steps):
+print("Accuracy:", accuracy_score(y_test, y_pred))
+print(classification_report(y_test, y_pred))
 
-        z = X @ w + b
-
-        y_hat = sigmoid(z)
-
-
-        error = y_hat - y
-
-        dw = X.T @ error / n_samples + 2*lam*w
-
-        db = np.mean(error)
-
-        w -= lr * dw
-        b -= lr * db
-
-
-    return w, b
